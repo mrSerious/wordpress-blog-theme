b96aa771de811fd7b1b72168b49850add49dc513
Tersoo Paul Atsen <tersoo.atsen@outlook.com>
2020-05-30 09:36:40 +0100

    finish build site footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -10,15 +11,37 @@
  */
 
 ?>
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<div class="footer-pre">
+			<ul class="social-links list-inline">
+				<li class="list-inline-item"><i class="fab fa-facebook-f"></i></li>
+				<li class="list-inline-item"><i class="fab fa-twitter"></i></li>
+				<li class="list-inline-item"><i class="fab fa-instagram"></i></li>
+			</ul>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+			<?php wp_nav_menu(array(
+				'theme_location'    => 'menu-1',
+				'menu_id'           => 'footer-menu',
+				'depth'             => 2,
+				'container'         => 'div',
+				'container_class'   => 'footer-nav-menu',
+				'menu_class'        => 'nav justify-content-center',
+				'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+				'walker'            => new WP_Bootstrap_Navwalker(),
+			));
+			?>
+
+		</div>
+		<div class="rights">
+			<span>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</span>
+		</div>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
